0d19abb595399994aa49ce24035fcb812f5ae04b
Matt Hardwick <matthardwick@gmail.com>
2017-01-22 13:55:00 -0800

    css/html update
    added adjusted pictures, bottom home section and flex completed. added
    pace.js. css updated and media queried added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -39,7 +39,7 @@ const reload = browserSync.reload;
 
 // Lint JavaScript
 gulp.task('lint', () =>
-    gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
+    gulp.src(['app/scripts/**/*.js', '!node_modules/**', '!app/scripts/pace.js'])
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
@@ -110,8 +110,9 @@ gulp.task('scripts', () =>
         // Note: Since we are not using useref in the scripts build pipeline,
         //       you need to explicitly list your scripts here in the right order
         //       to be correctly concatenated
-        './app/scripts/main.js'
+        './app/scripts/main.js',
         // Other scripts
+        './app/scripts/pace.js'
     ])
     .pipe($.newer('.tmp/scripts'))
     .pipe($.sourcemaps.init())
